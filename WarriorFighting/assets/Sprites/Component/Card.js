df8efd7566b2d56f889149c37abbf7c7a9b2a002
Unknown <854323427@qq.com>
2017-07-22 17:13:21 +0800

    今天的成果，加了一些手牌事件，但是还没完成，第一次push不知道能不能成功，管他呢

---
+++ WarriorFighting/assets/Sprites/Component/Card.js
@@ -0,0 +1,20 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        //使用位置阈值 
+        usePositionY: 0,
+        //魔法消耗
+        manaConsume: 0
+    },
+
+    // use this for initialization
+    onLoad: function () {
+
+    },
+
+    // called every frame, uncomment this function to activate update callback
+    // update: function (dt) {
+
+    // },
+});
