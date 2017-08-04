e5cb38291c84665236d64380b7b9944f80358a3b
pkngpkng <451433874@qq.com>
2017-08-05 02:02:12 +0800

    把平台跳跃和爬楼梯做完了，不过后期是否考虑加个跳跃键？把爬楼梯和跳跃键分离？

--- WarriorFighting/assets/MainScene/Scripts/HeroControl.js
+++ WarriorFighting/assets/MainScene/Scripts/HeroControl.js
@@ -20,9 +20,16 @@ cc.Class({
     // use this for initialization
     onLoad: function () {
         this.speed = cc.v2(0,0);
-        this.jumping = true;//判断是否为条约状态
         this.isGoLeft = false;//判断是否向左走
         this.isGoRight = false;//判断是否向右走
+        this.isStand = false;//站立状态
+        this.isStandPlatform = false;//是否站在台上
+
+        this.climbSpeed  = cc.v2(0,0);
+        this.isClimbingUp = false;//判断是否为向上攀爬状态
+        this.isClimbingDown = false;//判断是否为向下攀爬状态
+        this.isClimbing = false;//是否正在攀爬
+        this.touchedLadder = null;
 
         //开启碰撞
         cc.director.getCollisionManager().enabled = true;
@@ -46,9 +53,24 @@ cc.Class({
                 break;
             case cc.KEY.w:
             case cc.KEY.up:
-                if (!this.jumping) {
-                    this.jumping = true;
+                if ((this.isStand || this.isStandPlatform) && this.touchedLadder === null) {
                     this.speed.y = this.jumpSpeed;
+                } else if (this.touchedLadder !== null) {
+                    this.isClimbing = true;
+                    this.isClimbingUp = true;
+                    this.node.x = this.touchedLadder.x;
+                    this.speed.y = 0;
+                    this.isStandPlatform = false;
+                }
+                break;
+            case cc.KEY.s:
+            case cc.KEY.down:
+                if (this.touchedLadder !== null) {
+                    this.isClimbing = true;
+                    this.isClimbingDown = true;
+                    this.node.x = this.touchedLadder.x;
+                    this.speed.y = 0;
+                    this.isStandPlatform = false;
                 }
                 break;
         }
@@ -64,14 +86,60 @@ cc.Class({
             case cc.KEY.right:
                 this.isGoRight = false;
                 break;
+            case cc.KEY.w:
+            case cc.KEY.up:
+                this.isClimbingUp = false;
+                if (this.touchedLadder !== null) {
+                    this.isStandPlatform = false;
+                }
+                break;
+            case cc.KEY.s:
+            case cc.KEY.down:
+                this.isClimbingDown = false;
+                if (this.touchedLadder !== null) {
+                    this.isStandPlatform = false;
+                }
+                break;
         }
     },
 
     onCollisionEnter: function (other, self) {
-        if (other.node.group === "Wall") {
-            console.log("touch the wall");
-            this.jumping = false;
+        //地面接触判断
+        if (other.node.group === "Ground") {
+            console.log("touch the ground");
+            this.isStand = true;
             this.speed.y = 0;
+            self.node.y = other.node.y + other.node.height / 2 + self.node.height / 2;
+        }
+
+        //爬梯接触判断
+        if (other.node.group === "Ladder") {
+            console.log("touch the ladder");
+            this.touchedLadder = other.node;
+        }
+
+        //平台接触判断
+        if (other.node.group === "Platform") {
+            if ((self.node.y > other.node.y) && (Math.abs(self.node.x - other.node.x) < other.node.width / 2) ) {
+                this.isStandPlatform = true;
+                this.speed.y = 0;
+            }
+            console.log("touch the Platform");
+        }
+    },
+
+    onCollisionExit: function (other, self) {
+        if (other.node.group === "Ground") {
+            this.isStand = false;
+        }
+
+        if (other.node.group === "Ladder") {
+            this.touchedLadder = null;
+            this.isClimbing = false;
+        }
+
+        if (other.node.group === "Platform") {
+            this.isStandPlatform = false;
         }
     },
 
@@ -85,14 +153,24 @@ cc.Class({
         } else if (this.isGoRight === true) {
             this.speed.x = this.moveSpeed;//向右
         }
+        //处理Y轴的攀爬速度
+        if (this.isClimbingUp === this.isClimbingDown) {//左右键同时按或不按，则不动
+            this.climbSpeed.y = 0;
+        } else if (this.isClimbingUp === true) {
+            this.climbSpeed.y = this.moveSpeed;//向上
+        } else if (this.isClimbingDown === true && !this.isStand) {
+            this.climbSpeed.y = -this.moveSpeed;//向下
+        } else {
+            this.climbSpeed.y = 0;
+        }
 
         //处理重力加速度
-        if (this.jumping) {
+        if (!this.isStand && !this.isClimbing && !this.isStandPlatform) {
             this.speed.y -= this.gravite * dt;//加速度对时间积分
         }
 
         //速度对时间的积分变为位移
-        this.node.x += this.speed.x * dt;
-        this.node.y += this.speed.y * dt;
+        this.node.x += (this.speed.x + this.climbSpeed.x) * dt;
+        this.node.y += (this.speed.y + this.climbSpeed.y) * dt;
     },
 });
